9ae0e341b442cb488c2e6eaece5f2f33918a6848
CarlosZR <carlos.z.r.78@gmail.com>
2021-03-24 09:36:55 +0100

    Login y register terminado

---
+++ lib/widgets/boton_azul.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class BotonAzul extends StatelessWidget {
+
+  final String text;
+  final Function onPressed;
+
+  const BotonAzul({
+    Key key, 
+    @required this.text, 
+    @required this.onPressed
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: this.onPressed,
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(Colors.blue),
+        shape: MaterialStateProperty.all(StadiumBorder()),
+        elevation: MaterialStateProperty.all(2),
+      ),
+      child: Container(
+        width: double.infinity,
+        height: 55,
+        child: Center(
+          child: Text(this.text, style: TextStyle(color: Colors.white, fontSize: 18),)
+        ),
+      )
+    );
+  }
+}
